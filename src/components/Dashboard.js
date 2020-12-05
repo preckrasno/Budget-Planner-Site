095deb44ffd81aa5533a40bf8c582d27fd8f9c1e
Dmytro Muntian <dev@nazh.me>
2020-12-05 20:08:00 +0200

    feat: auth

---
+++ src/components/Dashboard.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Card, Button, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
+import { Link, useHistory } from 'react-router-dom'
+
+
+export default function Dashboard() {
+    const [error, setError] = useState('')
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
+
+    async function handleLogout() {
+        setError('')
+
+        try {
+            await logout()
+            history.push('/login')
+        } catch (err) {
+            setError('Failed to log out')
+        }
+
+
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2>Profile</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email:</strong> {currentUser.email}
+                    <Link to="/update-profile" >Update Profile</Link>
+                </Card.Body>
+
+            </Card>
+            <div>
+                <Button variant="link" onClick={handleLogout}>Log Out</Button>
+            </div>
+        </>
+    )
+}
